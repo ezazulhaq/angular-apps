833a0c08260c25c905df498ca00d19dfae74d17d
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-02-06 11:18:29 +0530

    index components

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HeroComponent } from "./hero/hero.component";
+import { AboutComponent } from "./about/about.component";
+import { FactsComponent } from "./facts/facts.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ResumeComponent } from "./resume/resume.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ServicesComponent } from "./services/services.component";
+import { TestimonialsComponent } from "./testimonials/testimonials.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, FactsComponent, SkillsComponent, ResumeComponent, PortfolioComponent, ServicesComponent, TestimonialsComponent, ContactComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'portfolio';
+
 }
