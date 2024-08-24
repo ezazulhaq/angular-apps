2fe4bba0ed6c6fbc3b047a1fa9a86a1abb91ab7a
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-08-24 16:56:11 +0530

    salah app - prayer service added

---
+++ salah-app/src/app/service/prayer.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Coordinates, PrayerTimes, CalculationMethod } from 'adhan';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrayerService {
+
+  constructor() { }
+
+  public getPrayerTimes(latitude: number, longitude: number): NamazTimes {
+    const coordinates = new Coordinates(latitude, longitude);
+    const params = CalculationMethod.MuslimWorldLeague();
+    const date = new Date();
+    const prayerTimes = new PrayerTimes(coordinates, date, params);
+
+    return {
+      fajr: prayerTimes.fajr,
+      sunrise: prayerTimes.sunrise,
+      dhuhr: prayerTimes.dhuhr,
+      asr: prayerTimes.asr,
+      maghrib: prayerTimes.maghrib,
+      isha: prayerTimes.isha
+    };
+  }
+}
