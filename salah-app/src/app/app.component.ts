d2e9b8d31b78852c9af70f09b741590b59a22617
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-08-25 12:48:50 +0530

    salah app - componented added to app

--- salah-app/src/app/app.component.ts
+++ salah-app/src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SalahTimesComponent } from './salah-times/salah-times.component';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SalahTimesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'salah-app';
+  selectedLat!: number;
+  selectedLng!: number;
+  error: string = "";
+
+  constructor() {
+    this.getLocation();
+  }
+
+  getLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.selectedLat = position.coords.latitude;
+          this.selectedLng = position.coords.longitude;
+        },
+        (error) => {
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              this.error = "Please enable location permissions for this site in your browser settings.";
+              break;
+            case error.POSITION_UNAVAILABLE:
+              this.error = "Location information is unavailable.";
+              break;
+            case error.TIMEOUT:
+              this.error = "The request to get user location timed out.";
+              break;
+            default:
+              this.error = "An unknown error occurred.";
+              break;
+          }
+        }
+      );
+    } else {
+      this.error = "Geolocation is not supported by this browser.";
+    }
+  }
 }
