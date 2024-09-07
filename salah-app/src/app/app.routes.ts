82e2a46c6a1d552df627b63f8d19eaa8979cc5fc
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-09-07 11:25:30 +0530

    refractored

--- salah-app/src/app/app.routes.ts
+++ salah-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PrayerTimesComponent } from './prayer-times/prayer-times.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'prayer',
+        title: 'Prayer Times',
+        component: PrayerTimesComponent
+    },
+    {
+        path: '',
+        redirectTo: 'prayer',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'prayer'
+    }
+];
