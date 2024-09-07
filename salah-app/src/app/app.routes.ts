e9ae6d8385ff205ef7fecf08c1a5a3a17423a6cb
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-09-07 12:26:52 +0530

    components added to router

--- salah-app/src/app/app.routes.ts
+++ salah-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
+import { PrayerTimesComponent } from './prayer-times/prayer-times.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'prayer',
+        title: 'Prayer Times',
+        component: PrayerTimesComponent,
+        resolve: {
+            latitude: (route: ActivatedRouteSnapshot) => route.queryParams['latitude'],
+            longitude: (route: ActivatedRouteSnapshot) => route.queryParams['longitude']
+        }
+    },
+    {
+        path: '',
+        redirectTo: 'prayer',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'prayer'
+    }
+];
--- salah-app/src/app/mobile/menu/menu.component.ts
+++ salah-app/src/app/mobile/menu/menu.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-mobile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+  latitude = input.required<number>();
+  longitude = input.required<number>();
+
+  constructor(private router: Router) { }
+
+  onClickPrayer() {
+    this.router.navigate(
+      ["prayer"],
+      {
+        queryParams: {
+          latitude: this.latitude(),
+          longitude: this.latitude()
+        }
+      })
+  }
 
 }
