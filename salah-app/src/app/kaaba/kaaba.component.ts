033edd53ed7785be719bf21fffbda91252e31a38
ezazulhaq.it <ezazulhaq.it@gmail.com>
2024-09-10 11:17:51 +0000

    removed the unused code

--- salah-app/src/app/kaaba/kaaba.component.ts
+++ salah-app/src/app/kaaba/kaaba.component.ts
@@ -3,7 +3,6 @@ import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { SalahAppService } from '../service/salah-app.service';
 import { BehaviorSubject, Observable, Subscription, combineLatest } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 // Extended DeviceOrientationEvent interface to include webkitCompassHeading
 interface ExtendedDeviceOrientationEvent extends DeviceOrientationEvent {
@@ -14,8 +13,7 @@ interface ExtendedDeviceOrientationEvent extends DeviceOrientationEvent {
   selector: 'app-kaaba',
   standalone: true,
   imports: [
-    TitleCasePipe,
-    AsyncPipe
+    TitleCasePipe
   ],
   templateUrl: './kaaba.component.html',
   styleUrl: './kaaba.component.css',
@@ -30,10 +28,9 @@ export class KaabaComponent implements OnInit, OnDestroy {
   private compassSubscription: Subscription | null = null;
   compassDeg = signal<number>(0);
 
-  compassSvg: SafeHtml = '';
   private isIOS: boolean;
 
-  constructor(private kaabaService: SalahAppService, private sanitizer: DomSanitizer) {
+  constructor(private kaabaService: SalahAppService) {
     this.heading$ = new BehaviorSubject<number>(0);
     this.kaabaDirection$ = this.kaabaService.getKaabaDirection();
     this.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
@@ -96,52 +93,11 @@ export class KaabaComponent implements OnInit, OnDestroy {
         .subscribe(relativeDirection => {
           if (relativeDirection !== null) {
             this.compassDeg.set(relativeDirection);
-            this.updateCompassSvg(relativeDirection);
-            const compassElement = document.querySelector('.compass') as HTMLElement;
-            if (compassElement) {
-              compassElement.style.transform = `rotate(${relativeDirection}deg)`;
-            }
           }
         });
     } else {
       console.error('Device orientation is not supported by this device.');
     }
-  }
-
-  getDirection(degrees: number): string {
-    const directions = ['North', 'North-East', 'East', 'South-East', 'South', 'South-West', 'West', 'North-West'];
-    const index = Math.round(degrees / 45) % 8;
-    return directions[index];
-  }
-
-  updateCompassSvg(degrees: number) {
-    const compassSize = 200;
-    const arrowSize = 120;
-
-    const svgString = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="${compassSize}" height="${compassSize}" viewBox="0 0 ${compassSize} ${compassSize}">
-        <circle cx="${compassSize / 2}" cy="${compassSize / 2}" r="${compassSize / 2 - 5}" fill="none" stroke="white" stroke-width="2" />
-        <text x="${compassSize / 2}" y="26" text-anchor="middle" class="text-xl text-gray-200 font-semibold" fill="currentColor">
-          N
-        </text>
-        <text x="${compassSize / 2}" y="${compassSize - 14}" text-anchor="middle" class="text-xl text-gray-200 font-semibold" fill="currentColor">
-          S
-        </text>
-        <text x="${compassSize - 16}" y="${compassSize / 2}" text-anchor="middle" dominant-baseline="middle" class="text-xl text-gray-200 font-semibold" fill="currentColor">
-          E
-        </text>
-        <text x="20" y="${compassSize / 2}" text-anchor="middle" dominant-baseline="middle" class="text-xl text-gray-200 font-semibold" fill="currentColor">
-          W
-        </text>
-        <polygon points="${compassSize / 2},${(compassSize - arrowSize) / 2} ${compassSize / 2 - 8},${(compassSize + arrowSize) / 2} ${compassSize / 2 + 8},${(compassSize + arrowSize) / 2}"
-              class="text-red-600" fill="currentColor" transform="rotate(${degrees}, ${compassSize / 2}, ${compassSize / 2})" />
-      </svg>
-    `;
-
-    console.log(svgString);
-
-    this.compassSvg = this.sanitizer.bypassSecurityTrustHtml(svgString);
-
   }
 
 }
