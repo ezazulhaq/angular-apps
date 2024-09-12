932a7702c5bc5da133cadbd5e67d7f2ea6b8da62
ezazulhaq.it <ezazulhaq.it@gmail.com>
2024-09-12 12:48:12 +0000

    app to switch between dark and light theme

--- salah-app/src/app/app.component.ts
+++ salah-app/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrayerTimesComponent } from './prayer-times/prayer-times.component';
 import { MenuComponent } from './mobile/menu/menu.component';
+import { ThemeSelectorService } from './service/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,24 @@ import { MenuComponent } from './mobile/menu/menu.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() { }
+  isThemeDark = signal<boolean>(false);
 
+  constructor(protected themeSelector: ThemeSelectorService) {
+    this.themeSelector.setSystemTheme();
+  }
+
+  switchTheme(): void {
+    this.isThemeDark.set(!this.isThemeDark());
+    if (this.isThemeDark()) {
+      this.themeSelector.setDarkTheme();
+    } else {
+      this.themeSelector.setLightTheme();
+    }
+  }
+
+  ngOnInit(): void {
+    this.isThemeDark.set(this.themeSelector.currentTheme() === 'dark');
+  }
 }
