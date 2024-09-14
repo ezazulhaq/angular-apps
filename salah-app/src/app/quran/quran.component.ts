ba8b380c52fe009e06a93d6ac4a23d7ae6925a36
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-09-14 18:02:37 +0530

    app now have feature to read qur'an

--- salah-app/src/app/quran/quran.component.ts
+++ salah-app/src/app/quran/quran.component.ts
@@ -1,10 +1,15 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-quran',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [
+    PdfViewerModule,
+    TitleCasePipe
+  ],
   templateUrl: './quran.component.html',
   styleUrl: './quran.component.css',
   host: {
@@ -12,5 +17,12 @@ import { Component } from '@angular/core';
   }
 })
 export class QuranComponent {
+
+  pdfSrc = "doc/quran_13_line_color_coded.pdf";
+
+
+  onError(event: any) {
+    console.error('Error loading PDF', event);
+  }
 
 }
