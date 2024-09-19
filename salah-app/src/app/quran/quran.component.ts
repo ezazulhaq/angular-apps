9cbb6445d8f868d3d4c97b752b950debcd3a73e8
ezazulhaq.it <ezazulhaq.it@gmail.com>
2024-09-19 11:09:20 +0000

    logs added to debug issue of last page loaded

--- salah-app/src/app/quran/quran.component.ts
+++ salah-app/src/app/quran/quran.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, isPlatformBrowser, TitleCasePipe } from '@angular/common';
-import { AfterViewInit, Component, effect, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
-import { PDFProgressData, PdfViewerModule } from 'ng2-pdf-viewer';
+import { Component, effect, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { PDFDocumentProxy, PDFProgressData, PdfViewerModule } from 'ng2-pdf-viewer';
 import { environment } from '../../environments/environment';
 import { FormsModule } from '@angular/forms';
 
@@ -32,6 +32,7 @@ export class QuranComponent implements OnInit {
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     effect(() => {
+      console.log("Effect in Page 1 - ", this.getPageFromLocalStorage())
       this.savePageToLocalStorage(this.page());
     })
   }
@@ -44,7 +45,7 @@ export class QuranComponent implements OnInit {
         this.page.set(savedPage);
       }
     }
-    console.log("This is Page 2- ", this.page());
+    console.log("This is Page 2 - ", this.page());
   }
 
   nextPage() {
@@ -61,8 +62,9 @@ export class QuranComponent implements OnInit {
     console.error('Error loading PDF', event);
   }
 
-  afterLoadComplete(pdfData: any) {
-    this.page.set(this.getPageFromLocalStorage());
+  afterLoadComplete(pdfData: PDFDocumentProxy) {
+    const savedPage = this.getPageFromLocalStorage();
+    this.page.set(savedPage > pdfData.numPages ? pdfData.numPages : savedPage); // Ensure valid page number
     console.log("After Load Complete - This is Page - ", this.page());
     this.totalPages.set(pdfData.numPages);
     this.isLoaded.set(true);
@@ -85,6 +87,7 @@ export class QuranComponent implements OnInit {
 
   getPageFromLocalStorage(): number {
     if (isPlatformBrowser(this.platformId)) {
+      console.log("Get Page From Local Storage - This is Page - ", this.page());
       const savedPage = localStorage.getItem(this.storageKey);
       return savedPage ? +savedPage : 1; // Default to page 1 if no saved page found
     }
