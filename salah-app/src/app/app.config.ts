01b078994e3e0e9b847e8ec34e6190b456b56958
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-09-21 12:51:49 +0530

    app upgraded to support PWA

--- salah-app/src/app/app.config.ts
+++ salah-app/src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient()
+    provideHttpClient(),
+    provideServiceWorker(
+      'ngsw-worker.js',
+      {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+      }
+    )
   ]
 };
