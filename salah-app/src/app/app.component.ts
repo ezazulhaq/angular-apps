9a6776080c16f0cc8a35db3071a35ebae6f15894
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-09-22 11:56:31 +0530

    unused imports removed

--- salah-app/src/app/app.component.ts
+++ salah-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, computed, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrayerTimesComponent } from './prayer-times/prayer-times.component';
 import { MenuComponent } from './mobile/menu/menu.component';
