3fbce2f67cfc61d166e6102f6340f2989f627abe
ezazulhaq.it <ezazulhaq.it@gmail.com>
2024-10-07 13:48:36 +0000

    environment propeties to sync with production

--- salah-app/src/environments/environment.prod.ts
+++ salah-app/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 const s3Bucket = import.meta.env.NG_APP_S3_BUCKET
 const openStreetUrl = import.meta.env.NG_APP_OPEN_STREET_URL;
 const groqApiKey = import.meta.env.NG_APP_GROQ_API_KEY;
+const groqApiModel = import.meta.env.NG_APP_GROQ_API_MODEL;
 
 export const environment = {
     production: true,
@@ -8,5 +9,8 @@ export const environment = {
     api: {
         map: `${openStreetUrl}`
     },
-    groqApiKey: `${groqApiKey}`
+    groq: {
+        apikey: `${groqApiKey}`,
+        model: `${groqApiModel}`
+    },
 };
