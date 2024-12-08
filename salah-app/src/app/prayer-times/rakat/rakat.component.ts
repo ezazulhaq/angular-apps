7434b631e1d49d55d3924476469829c2952b8389
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-12-08 12:41:22 +0530

    rakat component added

---
+++ salah-app/src/app/prayer-times/rakat/rakat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rakat',
+  imports: [],
+  templateUrl: './rakat.component.html',
+  styleUrl: './rakat.component.css'
+})
+export class RakatComponent {
+
+}
---
+++ salah-app/src/app/prayer-times/rakat/rakat.contant.ts
@@ -0,0 +1,76 @@
+import { Rakats } from "./rakat.model";
+
+const rakats: Rakats[] = [
+    {
+        name: "fajr",
+        before_fard: {
+            sunnah: 2
+        },
+        fard: 2,
+        after_fard: {
+            sunnah: 0,
+            nafl: 0,
+            wajib: 0
+        }
+    },
+    {
+        name: "zuhr",
+        before_fard: {
+            sunnah: 4
+        },
+        fard: 4,
+        after_fard: {
+            sunnah: 2,
+            nafl: 2,
+            wajib: 0
+        }
+    },
+    {
+        name: "asr",
+        before_fard: {
+            sunnah: 4
+        },
+        fard: 4,
+        after_fard: {
+            sunnah: 0,
+            nafl: 0,
+            wajib: 0
+        }
+    },
+    {
+        name: "maghrib",
+        before_fard: {
+            sunnah: 0
+        },
+        fard: 3,
+        after_fard: {
+            sunnah: 2,
+            nafl: 2,
+            wajib: 0
+        }
+    },
+    {
+        name: "isha",
+        before_fard: {
+            sunnah: 4
+        },
+        fard: 4,
+        after_fard: {
+            sunnah: 2,
+            nafl: 2,
+            wajib: 3
+        }
+    },
+    {
+        name: "Jumuah",
+        before_fard: {
+            sunnah: 4
+        },
+        fard: 2,
+        after_fard: {
+            sunnah: "4+2",
+            nafl: 2,
+            wajib: 0
+        }
+    }
+];
