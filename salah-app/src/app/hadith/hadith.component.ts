0eb01057083b735809407b003b23e11c8624a3a5
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2024-12-21 14:36:58 +0530

    all the chapters listed

--- salah-app/src/app/hadith/hadith.component.ts
+++ salah-app/src/app/hadith/hadith.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SupabaseService } from '../service/supabase.service';
+import { HadithChapters } from './hadith.model';
 
 @Component({
   selector: 'app-hadith',
   imports: [
     CommonModule,
+    RouterLink,
   ],
   templateUrl: './hadith.component.html',
   styleUrl: './hadith.component.css',
@@ -15,20 +17,23 @@ import { SupabaseService } from '../service/supabase.service';
   }
 })
 export class HadithComponent {
-  chapterList = signal<any[]>([]);
+
+  chapterList = signal<HadithChapters[]>([]);
+
+  hadithSource = signal<string>("Sahih Bukhari");
 
   constructor(private readonly supabaseService: SupabaseService) { }
 
   ngOnInit(): void {
-    this.supabaseService.getHadithChaptersFromSource("Sahih Bukhari").subscribe(
-      {
-        next: (data: any) => {
-          this.chapterList.set(data.data);
-          console.log(JSON.stringify(data.data, null, 2));
-        },
-        error: (error: any) => console.log(error.error),
-        complete: () => console.log("complete")
-      }
-    );
+    this.supabaseService.getHadithChaptersFromSource(this.hadithSource())
+      .subscribe(
+        {
+          next: (data: any) => {
+            this.chapterList.set(data.data);
+          },
+          error: (error: any) => console.log(error.error),
+          complete: () => console.log("complete")
+        }
+      );
   }
 }
---
+++ salah-app/src/app/hadith/hadith.model.ts
@@ -0,0 +1,5 @@
+export interface HadithChapters {
+    id: string;
+    chapter_no: number;
+    chapter_name: string;
+}
