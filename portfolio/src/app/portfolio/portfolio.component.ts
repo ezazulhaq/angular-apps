3463bee10d34910872028525f958f52d06ef0baf
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-01-21 13:26:16 +0000

    app updated

--- portfolio/src/app/portfolio/portfolio.component.ts
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-portfolio',
-  standalone: true,
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+    selector: 'app-portfolio',
+    imports: [],
+    templateUrl: './portfolio.component.html',
+    styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
 
