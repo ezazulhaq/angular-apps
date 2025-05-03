9aa6f642d875f37f5c2764355b5a827d43f78688
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-05-03 09:03:22 +0000

    module components added to home

--- salah-app/src/app/home/home.component.ts
+++ salah-app/src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ModuleComponent } from './module/module.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterLink
+    ModuleComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
