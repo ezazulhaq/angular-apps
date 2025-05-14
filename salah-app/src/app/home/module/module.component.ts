dc9717e02029fa408400cffeb9f4aa7be3b76df6
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-05-14 03:30:20 +0000

    module icons are now moved to component

--- salah-app/src/app/home/module/module.component.ts
+++ salah-app/src/app/home/module/module.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { IconComponent } from '../../shared/icon/icon.component';
+import { module_icons, ModuleIcons } from './module.contant';
 
 @Component({
   selector: 'app-home-module',
   imports: [
-    RouterLink
+    IconComponent
   ],
   templateUrl: './module.component.html',
   styleUrl: './module.component.css'
 })
 export class ModuleComponent {
+
+  icons = signal<ModuleIcons[]>(module_icons); 
 
 }
