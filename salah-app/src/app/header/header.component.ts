8ffbf298414522b17c3a5f58fbdbc5ce029526f2
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-06-04 04:38:41 +0000

    menu transition while scrolling
    menu to hide when scrolled up and show on down

--- salah-app/src/app/header/header.component.ts
+++ salah-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, HostListener, inject, OnInit } from '@angular/core';
 import { HeaderService } from './header.service';
 
 @Component({
@@ -7,9 +7,47 @@ import { HeaderService } from './header.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   headerService = inject(HeaderService);
+
+  private lastScrollTop = 0;
+  private scrollThreshold = 10; // Minimum scroll distance to trigger hide/show
+  isHeaderVisible = true;
+
+  ngOnInit() {
+    this.lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+  }
+
+  @HostListener('window:scroll')
+  onScrollMenu() {
+    const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+    
+    // Avoid negative values when bouncing scroll on mobile
+    if (currentScrollTop < 0) {
+      return;
+    }
+    
+    const scrollDifference = Math.abs(currentScrollTop - this.lastScrollTop);
+    
+    // Only trigger if scroll distance is above threshold
+    if (scrollDifference > this.scrollThreshold) {
+      if (currentScrollTop > this.lastScrollTop) {
+        // Scrolling down - hide header
+        this.isHeaderVisible = false;
+      } else {
+        // Scrolling up - show header
+        this.isHeaderVisible = true;
+      }
+      
+      this.lastScrollTop = currentScrollTop;
+    }
+    
+    // Always show header when at the top of the page
+    if (currentScrollTop <= 0) {
+      this.isHeaderVisible = true;
+    }
+  }
 
   toggleMenu() {
     this.headerService.toggleMenu();
