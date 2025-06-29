ebaa9cd00d30bcea494d6a80d3e07b4af9defa28
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2025-06-29 17:05:59 +0530

    check for access token record for active authgaurd

--- salah-app/src/app/guard/auth.gaurd.ts
+++ salah-app/src/app/guard/auth.gaurd.ts
@@ -1,12 +1,15 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../service/auth.service";
+import { environment } from "../../environments/environment";
 
 export const authGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
     const router = inject(Router);
+    const sbProjectId = environment.supabase.projectId
 
-    if (authService.isLoggedIn()) {
+    const access_token = localStorage.getItem(`sb-${sbProjectId}-auth-token`);
+    if (access_token || authService.isLoggedIn()) {
         return true;
     }
 
