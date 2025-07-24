6fe1ae0ad3ce43602badf28e2fe66cf0d368ef4b
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-07-24 13:03:40 +0000

    implemented title on home

--- salah-app/src/app/home/home.component.ts
+++ salah-app/src/app/home/home.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { ModuleComponent } from './module/module.component';
 import { ChatbotComponent } from '../chatbot/chatbot.component';
 import { AuthService } from '../service/auth.service';
+import { TitleComponent } from '../shared/title/title.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     ModuleComponent,
-    ChatbotComponent
+    ChatbotComponent,
+    TitleComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
